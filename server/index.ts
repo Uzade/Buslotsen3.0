a67e90790a59272f1e0e2ee4b3ed4c2a258ca84e
Joschka <joschka4@gmx.de>
2022-01-03 18:40:53 +0100

    added cors for frontend

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,13 @@ import users from "./users";
 import entries from "./entries";
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from "cors";
 const app = express();
 const PORT = 8080;
+
+app.use(cors({
+    origin: "*",
+}))
 
 app.use(express.json());
 
--- server/users/functions.ts
+++ server/users/functions.ts
@@ -2,21 +2,20 @@
 import { PrismaClient } from "@prisma/client";
 
 import { Request, Response } from 'express-serve-static-core';
-import users from ".";
 
 const prisma = new PrismaClient();
 
-export async function getPassword (res: Response, id: number){
-  if(isNaN(id)){
+export async function getPassword(res: Response, id: number) {
+  if (isNaN(id)) {
     res.status(400).json({
-      request:{
+      request: {
         type: "getPassword",
         status: "not found",
         id: id,
       },
       message: "You didn't specify a valid id."
     });
-  }else{
+  } else {
     const password = await prisma.user.findUnique({
       where: {
         id: id,
@@ -25,9 +24,9 @@ export async function getPassword (res: Response, id: number){
         password: true
       }
     });
-    if(password == null){
+    if (password == null) {
       res.status(400).json({
-        request:{
+        request: {
           type: "getPassword",
           status: "not found",
           id: id,
@@ -35,94 +34,142 @@ export async function getPassword (res: Response, id: number){
         message: "You didn't specify a valid id."
       });
     }
-    else{
+    else {
       res.status(200).json({
-        request:{
-            type: "getPassword",
-            status: "done",
-            id: id,
-          },
+        request: {
+          type: "getPassword",
+          status: "done",
+          id: id,
+        },
         data: password?.password
       });
-    } 
+    }
   }
 }
 
-export async function deleteUser(req: Request, res: Response){
+export async function changePassword(req: Request, res: Response) {
+  //get resetKey
+  const resetKey = await prisma.user.findUnique({
+    select: {
+      resetKey: true,
+    },
+    where: {
+      id: req.body.id,
+    }
+  })
+  if(resetKey?.resetKey == null){
+    res.status(400).json({
+      request: {
+        type: "changePassword",
+        status: "invalid id",
+      }
+    })
+  }
+  //test if resetKey got assigned
+  else if(resetKey?.resetKey == "None"){
+    res.status(401).json({
+      request: {
+        type: "changePassword",
+        status: "you must first request a resetKey",
+      }
+    })
+  }
+  else if(resetKey?.resetKey != req.body.resetKey){
+    res.status(400).json({
+      request: {
+        type: "changePassword",
+        status: "wrong resetKey",
+      }
+    })
+  }
+  else{
+    const user = await prisma.user.update({
+      where: {
+        id: req.body.id
+      },
+      data: {
+        password: req.body.password1,
+        resetKey: "None"
+      }
+    })
+  }
+}
+
+export async function deleteUser(req: Request, res: Response) {
   const user = await prisma.user.findUnique({
+    where: {
+      id: req.body.id,
+    },
+    include: {
+      entries: true
+    }
+  })
+
+  if (user == null) {
+    res.status(400).json({
+      request: {
+        type: "deleteUser",
+        status: "not found",
+      },
+      message: "You didn't specify a valid id."
+    });
+  }
+  //TODO check for admin status, when admin is added
+
+  else {
+    //first delete all asociated posts:
+    let x = "0";
+    if (user.entries != []) { }
+    for (x in user.entries) {
+      await prisma.entry.delete({
+        where: {
+          id: user.entries[x].id
+        },
+      });
+    }
+    const deleteUser = await prisma.user.delete({
       where: {
         id: req.body.id,
       },
-      include:{
-        entries: true
-      }
-    })
-
-  if(user == null){
-      res.status(400).json({
-          request:{
-              type: "deleteUser",
-              status: "not found",
-            },
-          message: "You didn't specify a valid id."
-      });
-  }
-  //TODO check for admin status, when admin is added
-  
-  else{
-    //first delete all asociated posts:
-    let x = "0";
-    if(user.entries != []){}
-    for(x in user.entries){
-      await prisma.entry.delete({
-        where: {
-            id: user.entries[x].id
-        },
     });
-    }
-      const deleteUser = await prisma.user.delete({
-          where: {
-              id: req.body.id,
-          },
-      });
-      res.status(200).json({
-          request:{
-              type: "deleteUser",
-              status: "done",
-            },
-            data: deleteUser
-      })
+    res.status(200).json({
+      request: {
+        type: "deleteUser",
+        status: "done",
+      },
+      data: deleteUser
+    })
   }
 }
 
 export async function getAll(res: Response) {
-  
-    const allUsers = await prisma.user.findMany({
-      select:{
-        id: true,
-        email: true,
-        name: true
-      }
-    })
-    res.status(200).json({ 
-      data: allUsers
-    });
+
+  const allUsers = await prisma.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      name: true
+    }
+  })
+  res.status(200).json({
+    data: allUsers
+  });
 }
-  
-export async function newUser(req: Request, res: Response){
-    //console.log(req.body);
-    const newUser = await prisma.user.create({
-      data: {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      },
-    });
-    res.status(201).json({
-      request:{
-        type: "newUser",
-        status: "done",
-      },
-      data: newUser
+
+export async function newUser(req: Request, res: Response) {
+  //console.log(req.body);
+  const newUser = await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    },
+  });
+  res.status(201).json({
+    request: {
+      type: "newUser",
+      status: "done",
+    },
+    data: newUser
   })
 }
