6eb297084509c39df27af9ada5c60c8faf355b58
Joschka <joschka4@gmx.de>
2022-01-07 13:42:03 +0100

    added simple post page

---
+++ client/src/routes/post.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    let created = false;
+    let error = "";
+
+
+    function onSubmit(e) {
+
+        const data = {
+            title: e.target.title.value,
+            authorId: Number(e.target.id.value)
+        };
+
+        fetch("http://localhost:8080/entries/new", { method: 'POST', headers: {
+                'Content-Type': 'application/json'
+            }, body:JSON.stringify(data)})
+            .then(response => response.json())
+            .then(data => {
+                if(data.request.status == 'done'){
+                    created = true;
+                    error = "";
+                } //TODO implement error display after error messaages are implemented in backend
+                /*else if(data.request.status == 'error'){
+                    created = true;
+                    error = "";
+                }*/
+        });
+    }
+</script>
+
+<div>
+    <form on:submit|preventDefault={onSubmit}>
+        <div>
+            <label for="name">Titel:</label>
+            <input
+                type="text"
+                id="title"
+                name="title"
+                value=""
+            />
+        </div>
+        <div>
+            <label for="name">Authornummer:</label>
+            <input
+                type="number"
+                id="id"
+                name="id"
+                value=""
+            />
+        </div>
+        <button type="submit">Submit</button>
+        {#if created}
+	        <h4>Entry sucessfully created</h4>
+        {/if}
+    </form>
+
+</div>
+
+<style>
+
+</style>
