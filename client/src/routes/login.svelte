2f48d27421d7aebb58a9b5d6203cd00f71ed9847
Joschka <joschka4@gmx.de>
2022-01-17 20:10:44 +0100

    added login page

---
+++ client/src/routes/login.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	function onSubmit(e) {
+		const data = {
+			name: e.target.UID.value,
+			password: e.target.password.value
+		};
+
+		fetch('http://localhost:8080/users/login/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.request.status == 'done') {
+					console.log('Logged in!');
+					sessionStorage.setItem('UID', e.target.UID.value);
+					sessionStorage.setItem('password', e.target.password.value);
+					window.location.href = 'http://localhost:3000/';
+				} //TODO implement error display after error messaages are implemented in backend
+				/*else if(data.request.status == 'error'){
+                    created = true;
+                    error = "";
+                }*/
+			});
+	}
+</script>
+
+<div>
+	<form on:submit|preventDefault={onSubmit}>
+		<div>
+			<label for="name">Username</label> <br>
+			<input type="text" id="UID" name="UID" value="" />
+		</div>
+		<div>
+            <label for="name">Password</label> <br>
+			<input type="password" id="password" name="password" value="" />
+		</div>
+		<button type="submit">Submit</button>
+	</form>
+</div>
+
+<style>
+    
+</style>
