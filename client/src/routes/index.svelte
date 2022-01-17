7d7a8834a0de9d2d2f029aa6b87b7b50ffc41c20
Joschka <joschka4@gmx.de>
2022-01-17 20:11:44 +0100

    check for login + use password for request

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,28 +1,43 @@
-
 <script>
-    import Entry from "../entry.svelte"
+	if (typeof window !== 'undefined') {
+		const UID = sessionStorage.getItem('UID');
+		const password = sessionStorage.getItem('password');
 
-    let entries = [];
-    fetch("http://localhost:8080/entries/all", { method: 'GET'})
-    .then(response => response.json())
-    .then(data => {
-        entries = data.data;
-        //console.log(entries);
-    });
-    $: console.log(entries);
+		if (!UID || !password) {
+			window.location.href = 'http://localhost:3000/login';
+		}
+	}
 
+	import Entry from '../entry.svelte';
+
+	let entries = [];
+	fetch('http://localhost:8080/entries/all', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			requestor: {
+				name: 'KarlHeinz',
+				password: 'test12345'
+			}
+		})
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			entries = data.data;
+			//console.log(entries);
+		});
+	//$: console.log(entries);
 </script>
 
 <div>
-
-    {#each entries as entry}
-        <Entry author={entry.author.name} message={entry.title} createdAt={entry.createdAt}/>
-    {:else}
-        <p>loading...</p>
-    {/each}
+	{#each entries as entry}
+		<Entry author={entry.author.name} message={entry.title} createdAt={entry.createdAt} />
+	{:else}
+		<p>loading...</p>
+	{/each}
 </div>
 
-
 <style>
-    
 </style>
