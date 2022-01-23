17c5d038af3c244a32070d5dd0b87f5e96953646
Joschka <joschka4@gmx.de>
2022-01-23 16:19:28 +0100

    added permition check

--- server/users/functions/deleteUser.ts
+++ server/users/functions/deleteUser.ts
@@ -1,52 +1,76 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from 'express-serve-static-core';
+import checkPerm from "../permition";
 const prisma = new PrismaClient();
 
 async function deleteUser(req: Request, res: Response) {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: req.body.id,
+  if (!req.body.requestor.password || !req.body.requestor.name) {
+    res.status(401).json({
+      request: {
+        type: "getAll",
+        status: "no permition",
       },
-      include: {
-        entries: true
-      }
-    })
-  
-    if (user == null) {
-      res.status(400).json({
-        request: {
-          type: "deleteUser",
-          status: "not found",
-        },
-        message: "You didn't specify a valid id."
-      });
-    }
-    //TODO check for admin status, when admin is added
-  
-    else {
-      //first delete all asociated posts:
-      let x = "0";
-      if (user.entries != []) { }
-      for (x in user.entries) {
-        await prisma.entry.delete({
-          where: {
-            id: user.entries[x].id
-          },
-        });
-      }
-      const deleteUser = await prisma.user.delete({
+      message: "You specified the wrong login credentials."
+    });
+  }
+  else {
+    let value = await checkPerm(req.body.requestor.password, req.body.requestor.name, 0);
+    if (value) {
+      const user = await prisma.user.findUnique({
         where: {
           id: req.body.id,
         },
-      });
-      res.status(200).json({
-        request: {
-          type: "deleteUser",
-          status: "done",
-        },
-        data: deleteUser
+        include: {
+          entries: true
+        }
       })
+
+      if (user == null) {
+        res.status(400).json({
+          request: {
+            type: "deleteUser",
+            status: "not found",
+          },
+          message: "You didn't specify a valid id."
+        });
+      }
+      //TODO check for admin status, when admin is added
+
+      else {
+        //first delete all asociated posts:
+        let x = "0";
+        if (user.entries != []) { }
+        for (x in user.entries) {
+          await prisma.entry.delete({
+            where: {
+              id: user.entries[x].id
+            },
+          });
+        }
+        const deleteUser = await prisma.user.delete({
+          where: {
+            id: req.body.id,
+          },
+        });
+        res.status(200).json({
+          request: {
+            type: "deleteUser",
+            status: "done",
+          },
+          data: deleteUser
+        })
+      }
+    }
+    else{
+      res.status(401).json({
+        request: {
+          type: "getAll",
+          status: "no permition",
+        },
+        message: "You specified the wrong login credentials."
+      });
     }
   }
+}
 
-  export default deleteUser;
+export default deleteUser;
