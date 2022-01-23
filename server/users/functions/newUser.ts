a829ab155d3f9178cb2e87efa29af99c139bfaad
Joschka <joschka4@gmx.de>
2022-01-23 15:35:15 +0100

    check permition when creating new User + send permitionId for new User

--- server/users/functions/newUser.ts
+++ server/users/functions/newUser.ts
@@ -1,27 +1,58 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from 'express-serve-static-core';
+import checkPerm from '../permition';
 const prisma = new PrismaClient()
 const bcrypt = require('bcrypt');
 
 async function newUser(req: Request, res: Response) {
-    //console.log(req.body);
-    const hash = bcrypt.hash(req.body.password, 10);
-    console.log(await hash);
-    const newUser = await prisma.user.create({
-      data: {
-        name: req.body.name,
-        email: req.body.email,
-        password: await hash,
-        permitionId: req.body.permitionID,
+  if (!req.body.requestor.password || !req.body.requestor.name) {
+    res.status(401).json({
+      request: {
+        type: "getAll",
+        status: "no permition",
       },
+      message: "You specified the wrong login credentials."
     });
-    res.status(201).json({
+  }
+  else if (!req.body.password || !req.body.name || !req.body.email || !req.body.permitionId) {
+    res.status(400).json({
       request: {
         type: "newUser",
-        status: "done",
-      },
-      data: newUser
+        status: "missing parameter",
+      }
     })
   }
+  else {
+    let permition = await checkPerm(req.body.requestor.password, req.body.requestor.name, 0);
+    if (permition) {
+      const hash = bcrypt.hash(req.body.password, 10);
+      console.log(await hash);
+      const newUser = await prisma.user.create({
+        data: {
+          name: req.body.name,
+          email: req.body.email,
+          password: await hash,
+          permitionId: req.body.permitionId,
+        },
+      });
+      res.status(201).json({
+        request: {
+          type: "newUser",
+          status: "done",
+        },
+        data: newUser
+      })
+    }
+    else{
+      res.status(401).json({
+        request: {
+          type: "getAll",
+          status: "no permition",
+        },
+        message: "You specified the wrong login credentials."
+      });
+    }
+  }
+}
 
-  export default newUser;
+export default newUser;
