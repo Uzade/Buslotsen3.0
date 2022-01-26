5e2c314a988374e83964f5958a0cf930881d091d
Joschka <joschka4@gmx.de>
2022-01-26 20:45:31 +0100

    added authorId to store

--- client/src/store.js
+++ client/src/store.js
@@ -3,3 +3,4 @@ import { writable } from 'svelte/store';
 export const modalNumber = writable(null);
 export const modalPostId = writable(null);
 export const isAdmin = writable(false);
+export const authorId = writable(0);
